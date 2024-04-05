c913f62567e80e8efcf72a1f1f402cdabdb814f4
Afanasususer <samadiabdelouahab4@gmail.com>
2024-04-05 21:46:28 +0000

    new commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/home'
+import ErrorPage from './pages/ErrorPage'
+import Html from "./pages/html";
+import Css from "./pages/css";
+import JavaScript from "./pages/javaScript";
+// f smiya dyal import tedar tsemi li bghiti lmohim hiya li ghadi tdem biha f lfile kolo wdarouri ykon awel harf capital, w mn l2a7ssen semi b nefss smiya dyal lfunction li kayna f lfile li nta baghi thadar hit katweli dir auto import lrassha fach katkteb smiya f dak f dak element so mn l2a7ssen n5edmo b smiyat dyal lfunction dyalna
+
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <ErrorPage />
+    // errorElement: <h1>this page not available</h1>
+  },
+
+  {
+    path: "/html",
+    element: <Html/>,
+    errorElement: <ErrorPage />
+    // errorElement: <h1>this page not available</h1>
+  },
+
+  {
+    path: "/css",
+    element: <Css/>,
+    errorElement: <ErrorPage />
+    // errorElement: <h1>this page not available</h1>
+  },
+
+  {
+    path: "/javascript",
+    element: <JavaScript/>,
+    errorElement: <ErrorPage />
+    // errorElement: <h1>this page not available</h1>
+  },
+
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,7 @@
+
+export default function ErrorPage() {
+  return (
+    <div className="err">Oups this page is not available</div>
+    // fch bghit nesaq had xi sir l ay file dyal css b chart ykun merbout m3a index.html w nessaq lcoddat dyalk tma 
+  )
+}
