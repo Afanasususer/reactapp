36dd5ec663d992bb084032b22b764769591e7ede
Afanasususer <samadiabdelouahab4@gmail.com>
2024-04-05 13:42:27 +0000

    from new branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>version2  branch ok</h1>
+        <h1>version2  the new branch</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
